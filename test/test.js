8016e0d68a0df94274f5293378864d40ebbf50d1
Miguel Angel Carrasco <tlacaelelsoftware@gmail.com>
2019-05-19 20:01:18 -0500

    Refactoring testing to generalize graph tests

--- test/test.js
+++ test/test.js
@@ -1,117 +1,10 @@
 let expect = require('chai').expect;
 let astar = require('../src/main');
 
-describe('Astar algorithm', function () {
-    describe('find path using astar on a graph', function () {
-        it('should find path form start to goal on a graph', function () {
-            let graphNodes = {
-                "a": {
-                    "heuristicCost": 8,
-                    "neighbors": {
-                        "b": 1.5
-                    }
-                },
-                "b": {
-                    "heuristicCost": 7.5,
-                    "neighbors": {
-                        "a": 1.5,
-                        "c": 1,
-                        "g": 3.4
-                    }
-                },
-                "c": {
-                    "heuristicCost": 6.2,
-                    "neighbors": {
-                        "b": 1,
-                        "e": 1,
-                        "f": 2.6,
-                        "d": 2.9
-                    }
-                },
-                "d": {
-                    "heuristicCost": 4.7,
-                    "neighbors": {
-                        "c": 2.9,
-                        "h": 1.6,
-                        "g": 1.8
-                    }
-                },
-                "e": {
-                    "heuristicCost": 5.4,
-                    "neighbors": {
-                        "c": 1,
-                        "m": 3.5
-                    }
-                },
-                "f": {
-                    "heuristicCost": 5.3,
-                    "neighbors": {
-                        "c": 2.6,
-                        "j": 4.5
-                    }
-                },
-                "g": {
-                    "heuristicCost": 6.5,
-                    "neighbors": {
-                        "b": 3.4,
-                        "d": 1.8,
-                        "h": 2.5
-                    }
-                },
-                "h": {
-                    "heuristicCost": 4.6,
-                    "neighbors": {
-                        "g": 2.5,
-                        "d": 1.6,
-                        "i": 1,
-                        "j": 1.6
-                    }
-                },
-                "i": {
-                    "heuristicCost": 5.5,
-                    "neighbors": {
-                        "h": 1,
-                        "k": 3
-                    }
-                },
-                "j": {
-                    "heuristicCost": 3,
-                    "neighbors": {
-                        "h": 1.6,
-                        "f": 4.5,
-                        "k": 2
-                    }
-                },
-                "k": {
-                    "heuristicCost": 3.2,
-                    "neighbors": {
-                        "j": 2,
-                        "i": 3,
-                        "l": 3.5
-                    }
-                },
-                "l": {
-                    "heuristicCost": 1.5,
-                    "neighbors": {
-                        "k": 3.2,
-                        "n": 1.5
-                    }
-                },
-                "m": {
-                    "heuristicCost": 2.6,
-                    "neighbors": {
-                        "e": 3.5
-                    }
-                },
-                "n": {
-                    "heuristicCost": 0,
-                    "neighbors": {
-                        "l": 1.5
-                    }
-                }
-            };
+describe('Search algorithms on graphs', function () {
 
-            let astarImpl = {
+    let testGraph = function (searchAlgorithm, graphNodes, expectedPath) {
+            let path = searchAlgorithm({
                 start: "a",
                 goal: "n",
                 heuristicCostEstimate: function (node) {
@@ -126,20 +19,128 @@ describe('Astar algorithm', function () {
                 getDistance: function (node1, node2) {
                     return graphNodes[node1]["neighbors"][node2];
                 }
-            };
+            });
+            expect(path).to.eql(expectedPath);
+        };
 
-            let path = astar.findPathAstar(astarImpl);
+    describe('find path on a simple graph', function () {
+        let simpleGraph = {
+            "a": {
+                "heuristicCost": 8,
+                "neighbors": {
+                    "b": 1.5
+                }
+            },
+            "b": {
+                "heuristicCost": 7.5,
+                "neighbors": {
+                    "a": 1.5,
+                    "c": 1,
+                    "g": 3.4
+                }
+            },
+            "c": {
+                "heuristicCost": 6.2,
+                "neighbors": {
+                    "b": 1,
+                    "e": 1,
+                    "f": 2.6,
+                    "d": 2.9
+                }
+            },
+            "d": {
+                "heuristicCost": 4.7,
+                "neighbors": {
+                    "c": 2.9,
+                    "h": 1.6,
+                    "g": 1.8
+                }
+            },
+            "e": {
+                "heuristicCost": 5.4,
+                "neighbors": {
+                    "c": 1,
+                    "m": 3.5
+                }
+            },
+            "f": {
+                "heuristicCost": 5.3,
+                "neighbors": {
+                    "c": 2.6,
+                    "j": 4.5
+                }
+            },
+            "g": {
+                "heuristicCost": 6.5,
+                "neighbors": {
+                    "b": 3.4,
+                    "d": 1.8,
+                    "h": 2.5
+                }
+            },
+            "h": {
+                "heuristicCost": 4.6,
+                "neighbors": {
+                    "g": 2.5,
+                    "d": 1.6,
+                    "i": 1,
+                    "j": 1.6
+                }
+            },
+            "i": {
+                "heuristicCost": 5.5,
+                "neighbors": {
+                    "h": 1,
+                    "k": 3
+                }
+            },
+            "j": {
+                "heuristicCost": 3,
+                "neighbors": {
+                    "h": 1.6,
+                    "f": 4.5,
+                    "k": 2
+                }
+            },
+            "k": {
+                "heuristicCost": 3.2,
+                "neighbors": {
+                    "j": 2,
+                    "i": 3,
+                    "l": 3.5
+                }
+            },
+            "l": {
+                "heuristicCost": 1.5,
+                "neighbors": {
+                    "k": 3.2,
+                    "n": 1.5
+                }
+            },
+            "m": {
+                "heuristicCost": 2.6,
+                "neighbors": {
+                    "e": 3.5
+                }
+            },
+            "n": {
+                "heuristicCost": 0,
+                "neighbors": {
+                    "l": 1.5
+                }
+            }
+        };
 
-            expect(path).to.eql(["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
+        it('should find path form start to goal on a graph using a-star algorithm', function () {
+            testGraph(astar.findPathAstar, simpleGraph, ["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
+        });
 
-            path = astar.findPathIDAstar(astarImpl);
-
-            expect(path).to.eql(["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
+        it('should find path form start to goal on a graph using ida-star algorithm', function () {
+            testGraph(astar.findPathIDAstar, simpleGraph, ["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
         });
     });
     describe('find path using astar on a directed graph', function () {
-        it('should find path form start to goal on a directed graph', function () {
-            let graphNodes = {
+        let directedGraph = {
                 "a": {
                     "heuristicCost": 8,
                     "neighbors": {
@@ -228,29 +229,12 @@ describe('Astar algorithm', function () {
                 }
             };
 
-            let astarImpl = {
-                start: "a",
-                goal: "n",
-                heuristicCostEstimate: function (node) {
-                    return graphNodes[node]["heuristicCost"];
-                },
-                getNodeIndex: function (node) {
-                    return node;
-                },
-                getNeighbors: function (node) {
-                    return Object.keys(graphNodes[node]["neighbors"]);
-                },
-                getDistance: function (node1, node2) {
-                    return graphNodes[node1]["neighbors"][node2];
-                }
-            };
-            let path = astar.findPathAstar(astarImpl);
+        it('should find path form start to goal on a directed graph using a-star', function () {
+            testGraph(astar.findPathAstar, directedGraph, ["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
+        });
 
-            expect(path).to.eql(["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
-
-            path = astar.findPathIDAstar(astarImpl);
-
-            expect(path).to.eql(["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
+        it('should find path form start to goal on a directed graph using ida-star', function () {
+            testGraph(astar.findPathIDAstar, directedGraph, ["a", "b", "c", "d", "h", "j", "k", "l", "n"]);
         });
     });
 });
